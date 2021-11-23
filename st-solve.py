0f67b4ac5d769524ea540ce10d0340ededd0757f
Alan Collins <alan.collins@bath.edu>
2021-11-23 13:21:57 -0600

    Modified find_top() now find_top_bottom() also finds bottom pixel of grid.

--- st-solve.py
+++ st-solve.py
@@ -5,19 +5,22 @@ from PIL import Image
 import pytesseract
 import cv2
 
-def find_top(image):
-	"""Find the top of the grid of letters
+def find_top_bottom(image):
+	"""Find the top and bottom of the grid of letters
 	
-	Scans from the top of the image until it finds the first white pixel
-	after the band of colour at the top of the playspace.
+	Scans from the top of the image until it finds the first white or
+	blue pixel after the band of colour at the top of the playspace.
+
+	Repeat from the bottom. Search until first white or blue pixel with
+	no band to worry about.
 
 	Args:
 	  image (PIL.Image instance):
 		The image to be processed, read in using PIL.Image.open().
 	  
 	Returns:
-	  y (int):
-	    y location of top of first grid square
+	  (top, bottom) (tuple of ints):
+	    y location of top of first grid square an bottom of the last
 	"""
 
 	height = image.height
@@ -43,19 +46,31 @@ def find_top(image):
 		# If traversed_first == True and we see white or blue,
 		# we've finished.
 		if colour in [(255,255,255), (0, 174, 239), (0, 178, 239)]:
-			return y
+			top = y
+			break
 
 
 		# If we get this far, we're still in the coloured band up top.
 		y+=1
+
+	# Now find the bottom. It should be after the black and grey pixels
+	y = image.height-1
+
+	while y > 0:
+
+		colour = image.getpixel((10,y))
+
+		if colour in [(255,255,255), (0, 174, 239), (0, 178, 239)]:
+			bottom = y
+			break
+		y-=1
+
+	return (top, bottom)
 
 
 infile = sys.argv[1]
 
 image = Image.open(infile)
 
-print(find_top(image))
+print(find_top_bottom(image))
 
-
-# for x in range(390, 410):
-# 	print(image.getpixel((x,340)))
